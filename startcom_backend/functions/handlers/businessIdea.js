cdbce9164e43f449b8ae08d03a4b1c39d9a911ba
boiquach <s3695290@rmit.edu.vn>
2020-04-13 14:24:09 +0700

    business idea crud

---
+++ startcom_backend/functions/handlers/businessIdea.js
@@ -0,0 +1,76 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const db = admin.firestore();
+
+//get all business ideas
+exports.getAllBusinessIdeas = (req,res)=>{
+    const businessIdeas = []
+    return db.collection('BusinessIdea').get()
+        .then((query)=>{
+            query.forEach(doc => {
+                var idea = doc.data();
+                idea.id = doc.id;
+                businessIdeas.push(idea);
+            })
+            return res.json(businessIdeas)
+        })
+        .catch((error)=>{
+            console.log(error);
+            return res.json({error:error})
+        })
+}
+
+//get business idea by id
+exports.getBusinessIdeaById = (req,res) =>{
+    return db.collection('BusinessIdea').doc(req.params.id).get()
+        .then((doc)=>{
+            if(doc!==null){
+                var idea = doc.data();
+                idea.id = doc.id;
+                return res.json(idea);
+            }
+            return res.json({error: "Business Idea does not exist."});
+        })
+        .catch((error)=>{
+            console.log(error);
+            return res.json({error:error});
+        })
+}
+
+//post business idea
+exports.postBusinessIdea = (req,res)=>{
+    var businessIdea = req.body
+    return db.collection('BusinessIdea').add(businessIdea)
+        .then((doc)=>{
+            businessIdea.id = doc.id;
+            return res.json(businessIdea);
+        })
+        .catch((error)=>{
+            console.log(error);
+            return res.json({error:error});
+        })
+}
+
+//edit business idea
+exports.editBusinessIdea = (req,res)=>{
+    return db.collection('BusinessIdea').doc(req.params.id).update(req.body)
+        .then(()=>{
+            return res.json(req.body);
+        })
+        .catch((error)=>{
+            console.log(error)
+            return res.json({error:error})
+        })
+}
+
+//delete business idea
+exports.deleteBusinessIdea = (req,res)=>{
+    return db.collection('BusinessIdea').doc(req.params.id).delete()
+        .then(()=>{
+            return res.json({message:'successfully deleted'});
+        })
+        .catch((error)=>{
+            console.log(error);
+            return res.json({error:error})
+        })
+}
--- startcom_backend/functions/index.js
+++ startcom_backend/functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const app = require('express')();
+const cors = require('cors');
+app.use(cors())
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const {
+    getAllBusinessIdeas,getBusinessIdeaById,
+    postBusinessIdea,editBusinessIdea,deleteBusinessIdea} = require('./handlers/businessIdea')
+
+app.get("/get_all_business_ideas",getAllBusinessIdeas);
+app.get("/get_business_idea/:id",getBusinessIdeaById);
+app.post("/post_business_idea",postBusinessIdea);
+app.put("/edit_business_idea/:id",editBusinessIdea);
+app.delete("/delete_business_idea/:id",deleteBusinessIdea);
+
+exports.api = functions.region('asia-east2').https.onRequest(app);
