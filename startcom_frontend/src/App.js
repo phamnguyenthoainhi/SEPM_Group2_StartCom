8d6074a07d7eed4efba801f92b6f4a4466a1b8d6
phamnguyenthoainhi <s3695349@rmit.edu.vn>
2020-04-14 15:35:17 +0700

    Add Router

--- startcom_frontend/src/App.js
+++ startcom_frontend/src/App.js
@@ -6,17 +6,20 @@ import RegisterBI from './components/businessideas/RegisterBI';
 import BIS from './components/businessideas/BIS';
 import theme from './theme/theme'
 import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <Provider store={store }>
-        <ThemeProvider theme={theme}>
-        <div className="App">
-          <RegisterBI/>
-          <BIS />
-        </div>
-        </ThemeProvider>
-      </Provider>
+      <BrowserRouter>
+            <Provider store={store}>
+                <ThemeProvider theme={theme}>
+                    <div className="App">
+                      <Route exact path={'/registerBI'} render={(props) => <RegisterBI {...props} />} />
+                    </div>
+                </ThemeProvider>
+          </Provider>
+      </BrowserRouter>
+      
      
     );
   }
