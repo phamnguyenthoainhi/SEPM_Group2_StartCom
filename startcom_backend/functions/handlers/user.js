8870752845bb55a529df02d4a6aea8a30354ec6c
boiquach <s3695290@rmit.edu.vn>
2020-04-14 16:15:29 +0700

    edit profile, fetch investors/consultants, delete profile

--- startcom_backend/functions/handlers/user.js
+++ startcom_backend/functions/handlers/user.js
@@ -3,8 +3,8 @@ const firebase = require('../config/config');
 const db = admin.firestore()
 
 exports.signUp = (req,res) =>{
-    
-    return firebase.auth().createUserWithEmailAndPassword(req.body.email,req.body.password)
+    const user = req.body
+    return firebase.auth().createUserWithEmailAndPassword(user.email,user.password)
         // .then((cred)=>{
         //     // Promise(createUser(req.body));
         //     return cred.user.getIdToken();
@@ -13,7 +13,8 @@ exports.signUp = (req,res) =>{
         //     return res.json(token);
         // })
         .then(()=>{
-            return Promise(createUser(req.body));
+            delete user.password;
+            return Promise(createUser(user));
         })
         .catch((error)=>{
             console.log(error)
@@ -35,6 +36,75 @@ exports.signIn = (req,res) =>{
         })
 }
 
+exports.editProfile = (req,res) =>{
+    return db.collection('User').doc(req.params.id).update(req.body)
+        .then(()=>{
+            return res.json(req.body)
+        })
+        .catch((error)=>{
+            console.log(error);
+            return res.json(error);
+        })
+
+}
+
+exports.getAllInvestors = (req,res) =>{
+    const investors =[];
+    return db.collection('User').where('type','==','investor').get()
+        .then((query)=>{
+            query.forEach(doc=>{
+                var investor = doc.data();
+                investor.id = doc.id;
+                investors.push(investor);
+            })
+            return res.json(investors);
+        })
+        .catch((error)=>{
+            console.log(error);
+            return res.json(error);
+        })
+}
+
+exports.getAllConsultants = (req,res) => {
+    const consultants =[];
+    return db.collection('User').where('type','==','consultant').get()
+        .then((query)=>{
+            query.forEach(doc=>{
+                var consultant = doc.data();
+                consultant.id=doc.id;
+                consultants.push(consultant);
+            })
+            return res.json(consultants);
+        })
+        .catch((error)=>{
+            console.log(error);
+            return res.json(error);
+        })
+}
+
+exports.deleteAccount = (userRecord) =>{
+    return db.collection('User').where('email','==',userRecord.email).get()
+        .then((query)=>{
+            return deleteUser(query.docs[0].id);
+        })
+}
+
 function createUser(user){
-    return db.collection('User').doc(user.email).set({type:user.type});
+    return db.collection('User').add(user)
+        .then((doc)=> {return null})
+        .catch(error=>{
+            console.log(error);
+            return res.json(error);
+        })
+}
+
+function deleteUser(id){
+    return db.collection('User').doc(id).delete()
+        .then(()=>{
+            return null;
+        })
+        .catch(error=>{
+            console.log(error);
+            return res.json(error);
+        })
 }
