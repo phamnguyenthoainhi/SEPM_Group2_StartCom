a3f0109575921e154ef5225c7a13b9c571acda52
Nmt131199 <manhtrietvt@gmail.com>
2020-04-16 01:45:33 +0700

    Responsive Navbar

--- startcom_frontend/src/components/Footer.js
+++ startcom_frontend/src/components/Footer.js
@@ -15,7 +15,7 @@ const styles = (theme) => ({
     logoContainer: {
         [theme.breakpoints.down('sm')]: {
             textAlign: "center"
-        }
+        },
     },
     navContainer: {
         marginTop: 20,
