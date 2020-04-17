79c4ecf15e844a5954b1013e4751b9c453945e3f
phamnguyenthoainhi <s3695349@rmit.edu.vn>
2020-04-17 11:47:23 +0700

    Updated Authentication UI with current theme

--- startcom_frontend/src/App.js
+++ startcom_frontend/src/App.js
@@ -10,6 +10,9 @@ import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter, Route} from 'react-router-dom';
 import HomePage from './components/anonymoususers/homepage/HomePage';
+import Authentication from './components/authentication/Authentication';
+import Login from './components/authentication/Login';
+import SignUp from './components/authentication/SignUp';
 class App extends Component {
   render() {
   const theme = createMuiTheme(themeFile);
@@ -23,6 +26,11 @@ class App extends Component {
                       <Route exact path={'/'} render={(props) => <HomePage {...props} />} />
                       <Route exact path={'/nav'} render={(props) => <Navbar {...props} />} />
                       <Route exact path={'/footer'} render={(props) => <Footer {...props} />} />
+                      <Route exact path={'/auth'} render={(props) => <Authentication {...props} />} />
+                      <Route exact path={'/login'} render={(props) => <Login {...props} />} />
+                      <Route exact path={'/signup'} render={(props) => <SignUp {...props} />} />
+
+
 
                     </div>
                 </ThemeProvider>
