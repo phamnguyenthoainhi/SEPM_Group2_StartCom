d0695910f81a22f1e7e909d13dc5b2e685e2010d
1trantienan <s3699000@rmit.edu.vn>
2020-05-07 20:36:48 +0700

    Update Investor UI

--- startcom_frontend/src/components/Layout/InvestorTemplate.js
+++ startcom_frontend/src/components/Layout/InvestorTemplate.js
@@ -3,31 +3,138 @@ import { connect } from 'react-redux';
 import withStyles from '@material-ui/core/styles/withStyles';
 
 import { fetchInvestor } from "../../actions/investor/InvestorAction";
+
+//Material-UI
+import logo from '../../images/company_logo.png';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from "@material-ui/core/Typography";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import TextField from "@material-ui/core/TextField";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import { CardMedia } from '@material-ui/core';
+import CardActions from '@material-ui/core/CardActions';
+
 
 const styles = (theme) => ({
     cardWrapper: {
-        maxWidth: 275,
-        padding: 20
+        border: '.5px solid grey',
+        "&:hover": {
+            cursor: 'pointer',
+            border: '.5px solid grey',
+        },
+    },
+    cardImage: {
+        objectFit: 'cover',
+        width: '100%',
+        maxHeight: '100%',
 
     },
-    media: {
-        width: "200px",
-        height: "100px"
-    }
+    cardContent: {
+        padding: '10px 20px'
+    },
+    cardTitle: {
+        fontFamily: theme.font2,
+        fontWeight: 700,
+        lineHeight: 'normal',
+        [theme.breakpoints.down('sm')]: {
+            fontSize: 15
+        },
+        [theme.breakpoints.down('xs')]: {
+            fontSize: 14
+        }
+    },
+    cardDescription: {
+        fontFamily: theme.font2,
+        [theme.breakpoints.down('sm')]: {
+            fontSize: 13
+        },
+        [theme.breakpoints.down('xs')]: {
+            fontSize: 12
+        }
+    },
+    cardCategory: {
+        fontFamily: theme.font2,
+        fontWeight: 700,
+        margin: 0,
+        [theme.breakpoints.down('sm')]: {
+            fontSize: 15
+        },
+        [theme.breakpoints.down('xs')]: {
+            fontSize: 14
+        }
+    },
 
+    investorTrue: {
+        fontFamily: theme.font1,
+        fontSize: 13,
+        fontWeight: 600,
+        textTransform: 'uppercase',
+        color: theme.color.secondary,
+        [theme.breakpoints.down('sm')]: {
+            fontSize: 12
+        },
+        [theme.breakpoints.down('xs')]: {
+            fontSize: 11
+        }
+    },
+    investorFalse: {
+        fontFamily: theme.font1,
+        fontSize: 13,
+        fontWeight: 600,
+        textTransform: 'uppercase',
+        color: '#C75D5D',
+        [theme.breakpoints.down('sm')]: {
+            fontSize: 12
+        },
+        [theme.breakpoints.down('xs')]: {
+            fontSize: 11
+        }
+    },
+    consultantTrue: {
+        fontFamily: theme.font1,
+        fontWeight: 600,
+        backgroundColor: theme.color.secondary,
+        marginBottom: 20
+    },
+    consultantFalse: {
+        fontFamily: theme.font1,
+        fontWeight: 600,
+        backgroundColor: '#C75D5D',
+        color: theme.color.primary2,
+        marginBottom: 20
+    },
+    cardFunding: {
+        fontFamily: theme.font2,
+        [theme.breakpoints.down('sm')]: {
+            fontSize: 13
+        },
+        [theme.breakpoints.down('xs')]: {
+            fontSize: 12
+        }
+    },
+    value: {
+        fontFamily: theme.font2,
+        fontWeight: 700,
+        [theme.breakpoints.down('sm')]: {
+            fontSize: 13
+        },
+        [theme.breakpoints.down('xs')]: {
+            fontSize: 12
+        }
+    },
+    dividerTrue: {
+        borderRadius: 30,
+        padding: 3,
+        backgroundColor: theme.color.secondary
+    },
+    dividerFalse: {
+        borderRadius: 30,
+        padding: 3,
+        backgroundColor: '#C75D5D'
+    }
 });
+
 
 
 class InvestorTemplate extends Component {
@@ -55,38 +162,60 @@ class InvestorTemplate extends Component {
 
     render() {
         const { classes, investor } = this.props;
-        const { open } = this.state;
+        const { cardElevation } = this.state;
         return (
-            <Card className={classes.cardWrapper}>
-                <CardMedia
-                    className={classes.media}
-                    image={investor.image}
-                />
-                <CardContent>
+            <CardActionArea onMouseOver={this.onMouseOver}  onMouseOut={this.onMouseOut}>
+                    <Card className={classes.cardWrapper} elevation={cardElevation}>
+                    {investor.image ? (
+                        <CardMedia
+                            component="img"
+                            alt="Company Logo"
+                            height="140"
+                            image={investor.image}
+                            title="Company Logo"
+                            className={classes.cardImage}
+                        >
+                        </CardMedia>
+                    ) : (
+                        <CardMedia
+                            component="img"
+                            alt="Company Logo"
+                            height="140"
+                            image={logo}
+                            title="Company Logo"
+                            className={classes.cardImage}
+                        >
+                        </CardMedia>
+                    )}
+
+
+                    <CardContent className={classes.cardContent}>
+
                     <Typography gutterBottom variant="h5" component="h2">
-                        {investor.name}
-                    </Typography>
-                    <Typography gutterBottom variant="subtitle2">
-                        {investor.type}
-                    </Typography>
-                    <br />
-                    <Typography variant="subtitle2" color="textSecondary" component="p" gutterBottom>
-                        {investor.email}
-                    </Typography>
-                    <br />
-                    <Typography>
-                       {investor.email}
-                    </Typography>
-                    <Typography variant="subtitle2" component="h2">
-                       Verify: {investor.verified}
-                    </Typography>
-                </CardContent>
-                <CardActions>
+                    {investor.name}
+                </Typography>
+                <Typography gutterBottom variant="subtitle2">
+                    {investor.type}
+                </Typography>
+                <br />
+                <Typography variant="subtitle2" color="textSecondary" component="p" gutterBottom>
+                    {investor.email}
+                </Typography>
+                <br />
+                <Typography>
+                   {investor.email}
+                </Typography>
+                <Typography variant="subtitle2" component="h2">
+                   Verify: {investor.verified}
+                </Typography>
+                    </CardContent>
+                    <CardActions>
                     <Button variant="contained" onClick={this.handleClickOpen}>More Info</Button>
                 </CardActions>
-            </Card>
+                </Card>
+            </CardActionArea>
         )
-    };
+    }
 }
 const mapDispatchToProps = dispatch => ({
     // updateBI: (businessIdea, id) => dispatch(updateBI(businessIdea, id)),
