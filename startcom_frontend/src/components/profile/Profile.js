f79b92e1065fc877fae50964527228dc48d4bcf4
Nmt131199 <manhtrietvt@gmail.com>
2020-05-09 22:19:52 +0700

    before merging w boi_backend

--- startcom_frontend/src/components/profile/Profile.js
+++ startcom_frontend/src/components/profile/Profile.js
@@ -16,13 +16,7 @@ import Card from "@material-ui/core/Card";
 import IconButton from "@material-ui/core/IconButton";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import BITemplateProfile from "../Layout/BITemplateProfile";
-
-
-// const ColorCircularProgress = withStyles({
-//     root: {
-//         color: '#3C5155'
-//     },
-// })(CircularProgress);
+import {getUser} from "../../actions/users/UserActions";
 
 const styles = (theme) => ({
     containerWrapper: {
@@ -237,10 +231,12 @@ const mapDispatchToProps = dispatch => ({
     updateBI: (businessIdea,id) => dispatch(updateBI(businessIdea,id)),
     deleteBI: (id) => dispatch(deleteBI(id)),
     getBI: (id) => dispatch(getBI(id)),
+    getUser: (id) => dispatch(getUser(id))
 
 });
 
 const mapStateToProps = state => ({
+    user: state.usersReducer.user,
     businessIdea: state.businessIdeasData.businessIdea,
     loading: state.businessIdeasData.loading,
 });
